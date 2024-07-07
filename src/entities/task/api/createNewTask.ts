9d1f061013453dfb5097c2bd4a6846fe8bdb2c9a
alex <easttem@gmail.com>
2024-07-07 14:58:04 +0300

    refactor: post tasks request

--- src/entities/task/api/createNewTask.ts
+++ src/entities/task/api/createNewTask.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-export function createNewTask(title: string, description: string) {
-  axios({
-    method: "post",
-    url: "http://localhost:5000/tasks",
-    data: {
-      title: title,
-      description: description,
-    },
-  });
-}
+export const createNewTask = async (title: string, description: string) => {
+  try {
+    await axios.post("http://localhost:5000/tasks", {
+      data: {
+        title: title,
+        description: description,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
