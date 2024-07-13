c140e25c80f0f42db25a6fd3a6daa721b1e2790c
EastTemplar <easttem@gmail.com>
2024-07-13 15:32:55 +0300

    fix: tasks holder gap

--- src/shared/ui/paper/paper.tsx
+++ src/shared/ui/paper/paper.tsx
@@ -3,9 +3,5 @@ import MUIPaper, { PaperProps } from "@mui/material/Paper/Paper";
 export function Paper(props: PaperProps) {
   const { children } = props;
 
-  return (
-    <MUIPaper square={false} {...props}>
-      {children}
-    </MUIPaper>
-  );
+  return <MUIPaper {...props}>{children}</MUIPaper>;
 }
