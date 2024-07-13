4f2021d46e0a0e130caeb5e8810f3df2f6eb7ccd
EastTemplar <easttem@gmail.com>
2024-07-13 16:37:45 +0300

    fix: markdonetask request signature

--- src/entities/task/api/markDoneTask.ts
+++ src/entities/task/api/markDoneTask.ts
@@ -2,13 +2,15 @@ import axios from "axios";
 
 export const markDoneTask = async (index: number, isDone: boolean) => {
   try {
-    return await axios.patch("http://localhost:5000/tasks", {
+    return axios({
+      method: "patch",
+      url: "http://localhost:5000/tasks",
       data: {
         id: index,
         isDone: isDone,
       },
     });
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
