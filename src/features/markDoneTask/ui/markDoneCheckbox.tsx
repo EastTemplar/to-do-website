a4661e55f13cdd7ccf059f044d53e1708fefb9bb
EastTemplar <easttem@gmail.com>
2024-07-13 16:49:42 +0300

    fix: checkbox toggling

--- src/features/markDoneTask/ui/markDoneCheckbox.tsx
+++ src/features/markDoneTask/ui/markDoneCheckbox.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 import { markDoneTask } from "../../../entities/task/api/markDoneTask";
 import { Checkbox } from "../../../shared/ui/checkbox";
 
@@ -6,12 +6,11 @@ interface IMarkDoneCheckboxProps {
   taskId: number;
 }
 export function MarkDoneCheckbox(props: IMarkDoneCheckboxProps) {
-  const [isToggled, Toggle] = useState(false);
+  const [isToggled, setIsToggled] = useState(true);
   const { taskId } = props;
 
-  function toggleMark(event: MouseEvent<HTMLButtonElement>): void {
-    event.preventDefault();
-    Toggle(!isToggled);
+  function toggleMark() {
+    setIsToggled(!isToggled);
     markDoneTask(taskId, isToggled);
   }
 
